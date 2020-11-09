3a23ef9a1cbe3be07dec7bbe030b2fa45b68af8a
Robert Horngren <robert@horngren.net>
2020-11-09 09:52:42 +0100

    added firebase database

--- app/src/main/java/com/example/octochat/MainActivity.kt
+++ app/src/main/java/com/example/octochat/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.octochat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
     }
 }
