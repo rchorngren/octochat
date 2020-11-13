f3e60901a5f31b5dd2a9469750b9b8ad845c1199
Robert Horngren <robert@horngren.net>
2020-11-13 14:16:49 +0100

    rebased branch upon master

--- app/src/main/java/com/example/octochat/MainActivity.kt
+++ app/src/main/java/com/example/octochat/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var auth: FirebaseAuth
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
