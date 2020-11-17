3a8aed49458999f7a5482180031fb301fb27cb32
Robert Horngren <robert@horngren.net>
2020-11-17 11:12:54 +0100

    rebase upon master

--- app/src/main/java/com/example/octochat/SettingsActivity.kt
+++ app/src/main/java/com/example/octochat/SettingsActivity.kt
@@ -1,21 +1,55 @@
 package com.example.octochat
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.LinearLayout
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.android.synthetic.main.activity_settings.*
-
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var navigateBackButton : ImageView
+    private lateinit var changePasswordButton : ConstraintLayout
+    private lateinit var logoutConstraint : ConstraintLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        navigateBackButton = findViewById(R.id.navigateBackButton)
+        changePasswordButton = findViewById(R.id.changePasswordConstraint)
+        logoutConstraint = findViewById(R.id.logoutConstraint)
 
+
+        navigateBackButton.setOnClickListener {
+            finish()
+        }
+
+        changePasswordButton.setOnClickListener{
+            //Toggle changePassword fragment
+            displayChangePasswordFragment()
+        }
+
+        logoutConstraint.setOnClickListener{
+            //Logout current user
+            Log.d("!!!", "Click!")
+        }
     }
 
+    fun displayChangePasswordFragment() {
+        val changePasswordFragment = ChangePasswordFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.changePasswordContainer, changePasswordFragment, "changePasswordFragment")
+        transaction.commit()
+    }
+
+    fun removeChangePasswordFragment() {
+        val changePasswordFragment = supportFragmentManager.findFragmentByTag("changePasswordFragment")
+        if(changePasswordFragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.remove(changePasswordFragment)
+            transaction.commit()
+        }
+    }
 }
